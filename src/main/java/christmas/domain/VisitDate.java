ebdf2c1c25239c2ad8992014c38b1fdddbd149bb
SungKyum Kim <sungkyum1@naver.com>
2023-11-11 18:05:08 +0900

    feat(VisitDate): 입력 날짜 검증 기능 구현

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -1,4 +1,20 @@
-package christmas.domain.menu;
+package christmas.domain;
 
 public class VisitDate {
+    private final int date;
+
+    public VisitDate(int date) {
+        validateDate(date);
+        this.date = date;
+    }
+
+    private void validateDate(int date) {
+        if (isOutOfRangeInteger(date)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isOutOfRangeInteger(int date) {
+        return date < 1 || date > 31;
+    }
 }
