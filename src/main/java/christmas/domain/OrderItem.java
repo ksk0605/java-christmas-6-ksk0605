3fb3d239508d852ac3bd4e6dbaa7f36547e329fb
SungKyum Kim <sungkyum1@naver.com>
2023-11-12 03:26:35 +0900

    feat(OrderItem): 주문 메뉴 개수 검증 기능 구현

--- src/main/java/christmas/domain/OrderItem.java
+++ src/main/java/christmas/domain/OrderItem.java
@@ -1,2 +1,32 @@
-package christmas.domain;public class OrderItem {
+package christmas.domain;
+
+import christmas.domain.menu.MenuItem;
+
+public class OrderItem {
+    private final MenuItem menuItem;
+    private final int menuCount;
+
+    private OrderItem(MenuItem menuItem, int menuCount) {
+        validateMenuCount(menuCount);
+        this.menuItem = menuItem;
+        this.menuCount = menuCount;
+    }
+
+    private static boolean isOutOfRangeMenuCount(int menuCount) {
+        return menuCount < 1;
+    }
+
+    public static OrderItem from(MenuItem menuItem, int menuCount) {
+        return new OrderItem(menuItem, menuCount);
+    }
+
+    private void validateMenuCount(int count) {
+        if (isOutOfRangeMenuCount(count)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public MenuItem getMenuItem() {
+        return menuItem;
+    }
 }
