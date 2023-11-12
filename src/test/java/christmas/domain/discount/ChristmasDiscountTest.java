b17f23f66ea6f0f5df6d47cbcf1389efb0703f95
SungKyum Kim <sungkyum1@naver.com>
2023-11-12 23:13:11 +0900

    test(ChristmasDiscount): 크리스마스 디데이 할인 금액 계산 기능 테스트 코드 구현

--- src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
+++ src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
@@ -1,4 +1,30 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.domain.discount;
+
+import christmas.domain.VisitDate;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderItems;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ChristmasDiscountTest {
-  
+    ChristmasDiscount christmasDiscount = new ChristmasDiscount();
+
+    @DisplayName("크리스마스 디데이 할인 금액을 계산한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1,1000", "15,2400", "25,3400"})
+    void calculateDiscountAmount(int date, int amount) {
+        // given
+        VisitDate visitDate = new VisitDate(date);
+        OrderItems orderItems = new OrderItems(new String[]{"샴페인-1", "초코케이크-2"});
+        Order order = new Order(orderItems, visitDate);
+
+        // when
+        int discountAmount = christmasDiscount.calculateDiscountAmount(order);
+
+        // then
+        assertThat(discountAmount).isEqualTo(amount);
+    }
 }
