509136b0ec7169040d5dfcf5b40d592da8262293
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 02:17:21 +0900

    test(InputView): 방문 날짜 입력 검증 기능 테스트 코드 구현

---
+++ src/test/java/christmas/view/InputViewTest.java
@@ -0,0 +1,32 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+    @BeforeEach
+    void resetBuffer() {
+        Console.close();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"팔", "2,1", "@##@$"})
+    @DisplayName("날짜는 하나의 정수만 입력할 수 있다.")
+    void readDateWithInvalidInput(String input) {
+        setInput(input);
+        assertThatThrownBy(() -> InputView.readDate())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void setInput(String input) {
+        final byte[] buf = input.getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+}
