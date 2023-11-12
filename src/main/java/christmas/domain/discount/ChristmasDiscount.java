2bb2e95a26f64b8f3a3e782f4e142098c699e501
SungKyum Kim <sungkyum1@naver.com>
2023-11-12 23:14:13 +0900

    feat(ChristmasDiscount): 크리스마스 디데이 할인 금액 계산 기능 구현

--- src/main/java/christmas/domain/discount/ChristmasDiscount.java
+++ src/main/java/christmas/domain/discount/ChristmasDiscount.java
@@ -1,2 +1,16 @@
-package christmas.domain.discount;public class ChristmasDiscount {
+package christmas.domain.discount;
+
+import christmas.domain.order.Order;
+
+public class ChristmasDiscount implements Discount {
+    @Override
+    public int calculateDiscountAmount(Order order) {
+        int visitDate = order.getVisitDate();
+
+        if (visitDate > 25) {
+            return 0;
+        }
+
+        return 1000 + (100 * (visitDate - 1));
+    }
 }
