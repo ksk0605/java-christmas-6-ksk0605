3f1b7468f6148636b2a0c0cbe00ab9e1ca29c9d6
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 03:27:01 +0900

    feat(OutputView): 주문 메뉴 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,2 +1,15 @@
-package christmas.view;public class OutputView {
+package christmas.view;
+
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderItem;
+import christmas.domain.order.OrderItems;
+
+public class OutputView {
+    public static void printMenu(Order order) {
+        System.out.println("\n<주문 메뉴>");
+        OrderItems orderItems = order.getOrderItems();
+        for (OrderItem orderItem : orderItems.getItems()) {
+            System.out.println(orderItem.getMenuItem() + " " + orderItem.getMenuCount() + "개");
+        }
+    }
 }
