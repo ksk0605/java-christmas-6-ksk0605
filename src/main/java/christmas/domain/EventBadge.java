7d9964b56e32f61717bc7cc65ba797a3dcd22925
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 01:50:42 +0900

    feat(EventBadge): 총 혜택 금액에 따라 다른 이벤트 배지 부여 기능 구현

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -1,2 +1,26 @@
-package christmas.domain;public class EventBadge {
+package christmas.domain;
+
+import java.util.Arrays;
+
+public enum EventBadge {
+    별(5000),
+    트리(10000),
+    산타(20000);
+
+    private final int minPurchaseAmount;
+
+    EventBadge(int minPurchaseAmount) {
+        this.minPurchaseAmount = minPurchaseAmount;
+    }
+
+    public int getMinPurchaseAmount() {
+        return minPurchaseAmount;
+    }
+
+    public static EventBadge getBadgeForAmount(int purchaseAmount) {
+        return Arrays.stream(values())
+                .filter(badge -> purchaseAmount >= badge.getMinPurchaseAmount())
+                .reduce((first, second) -> second)
+                .orElse(null);
+    }
 }
