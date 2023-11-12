1d7c5d4a50a25f6e02c0cb2448a1d1323029a550
SungKyum Kim <sungkyum1@naver.com>
2023-11-12 23:51:30 +0900

    refactor(in Discount Package): private 함수 분리로 계산 로직 개선

--- src/main/java/christmas/domain/discount/ChristmasDiscount.java
+++ src/main/java/christmas/domain/discount/ChristmasDiscount.java
@@ -3,14 +3,22 @@ package christmas.domain.discount;
 import christmas.domain.order.Order;
 
 public class ChristmasDiscount implements Discount {
+    private static boolean isNotDiscountDate(int visitDate) {
+        return visitDate > 25;
+    }
+
+    private static int calculateAmount(int visitDate) {
+        return 1000 + (100 * (visitDate - 1));
+    }
+
     @Override
     public int calculateDiscountAmount(Order order) {
         int visitDate = order.getVisitDate();
 
-        if (visitDate > 25) {
+        if (isNotDiscountDate(visitDate)) {
             return 0;
         }
 
-        return 1000 + (100 * (visitDate - 1));
+        return calculateAmount(visitDate);
     }
 }
--- src/main/java/christmas/domain/discount/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -4,14 +4,22 @@ import christmas.domain.order.Order;
 
 public class WeekdayDiscount implements Discount {
 
+    private static boolean isNotWeekDayDate(int visitDate) {
+        return visitDate % 7 != 1 && visitDate % 7 != 2;
+    }
+
+    private static int calculateAmount(Order order) {
+        return 2023 * order.orderItemCounts();
+    }
+
     @Override
     public int calculateDiscountAmount(Order order) {
         int visitDate = order.getVisitDate();
 
-        if (visitDate % 7 != 1 && visitDate % 7 != 2) {
+        if (isNotWeekDayDate(visitDate)) {
             return 0;
         }
 
-        return 2023 * order.orderItemCounts();
+        return calculateAmount(order);
     }
 }
