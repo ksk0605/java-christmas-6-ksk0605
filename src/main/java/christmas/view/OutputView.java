1628783301f2a335f3176b75d0ad58df75780b31
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 03:41:02 +0900

    feat(OutputView): 할인 전 총주문 금액 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -4,6 +4,8 @@ import christmas.domain.order.Order;
 import christmas.domain.order.OrderItem;
 import christmas.domain.order.OrderItems;
 
+import java.text.NumberFormat;
+
 public class OutputView {
     public static void printMenu(Order order) {
         System.out.println("\n<주문 메뉴>");
@@ -11,5 +13,13 @@ public class OutputView {
         for (OrderItem orderItem : orderItems.getItems()) {
             System.out.println(orderItem.getMenuItem() + " " + orderItem.getMenuCount() + "개");
         }
+    }
+
+    public static void printOrderAmount(Order order) {
+        System.out.println("\n<할인 전 총주문 금액>\n" + formatPrizeAmount(order) + "원");
+    }
+    
+    private static String formatPrizeAmount(Order order) {
+        return NumberFormat.getIntegerInstance().format(order.calculateOrderAmount());
     }
 }
