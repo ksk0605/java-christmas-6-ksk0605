d25d842098d89ae9515fd5f21e7953f1ecddbf5a
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 00:47:55 +0900

    feat(SpecialDiscount, SpecialDiscountDay): 특별 할인 금액 계산 기능 구현

--- src/main/java/christmas/domain/discount/SpecialDiscount.java
+++ src/main/java/christmas/domain/discount/SpecialDiscount.java
@@ -1,2 +1,19 @@
-package christmas.domain.discount;public class SpecialDiscount {
+package christmas.domain.discount;
+
+import christmas.domain.order.Order;
+
+public class SpecialDiscount implements Discount {
+
+    @Override
+    public int calculateDiscountAmount(Order order) {
+        int visitDate = order.getVisitDate();
+        if (isNotSpecialDiscountDay(visitDate)) {
+            return 0;
+        }
+        return 1000; // TODO : static 상수 분리
+    }
+
+    private static boolean isNotSpecialDiscountDay(int visitDate) {
+        return !SpecialDiscountDay.isSpecialDiscountDay(visitDate);
+    }
 }
