9a210f341dd3b6095f7a3b19c809b1bc66cdb824
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 03:27:41 +0900

    feat(Order): 주문 아이템 반환 기능 추가

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -22,4 +22,8 @@ public class Order {
     public int orderItemCounts() {
         return orderItems.getOrderItemCountAmount();
     }
+
+    public OrderItems getOrderItems() {
+        return orderItems;
+    }
 }
