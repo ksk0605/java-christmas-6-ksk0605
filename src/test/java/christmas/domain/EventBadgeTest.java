508f3ec3068ce49413c4aac22d3b928b7753c882
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 01:50:14 +0900

    test(EventBadge): 총 혜택 금액에 따른 이벤트 배지 부여 기능 테스트 코드 구현

---
+++ src/test/java/christmas/domain/EventBadgeTest.java
@@ -0,0 +1,30 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class EventBadgeTest {
+    @DisplayName("총 혜택 금액에 따라 다른 이벤트 배지를 부여한다.")
+    @ParameterizedTest
+    @CsvSource({
+            "5000, 별",
+            "10000, 트리",
+            "20000, 산타",
+            "6000, 별",
+            "15000, 트리"
+    })
+    public void getBadgeForAmount(int amount, EventBadge expectedBadge) {
+        assertEquals(expectedBadge, EventBadge.getBadgeForAmount(amount));
+    }
+
+    @DisplayName("총 할인 금액이 5000원 미만 일 경우에는 배지를 지급하지 않는다.")
+    @Test
+    public void getBadgeForAmountWithBelowMinimum() {
+        assertNull(EventBadge.getBadgeForAmount(4999));
+    }
+}
