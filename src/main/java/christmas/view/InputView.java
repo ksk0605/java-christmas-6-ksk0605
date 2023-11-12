430321e04c04ce9a87a4dc5f4e2727f4c4e5e7d2
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 02:17:49 +0900

    feat(InputView): 날짜 입력 및 검증 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,2 +1,35 @@
-package christmas.view;public class InputView {
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public static int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = getUserInputDate();
+        validateInput(input);
+        return toInt(input);
+    }
+
+    private static String getUserInputDate() {
+        return Console.readLine();
+    }
+
+    private static int toInt(String input) {
+        return Integer.parseInt(input);
+    }
+
+    private static void validateInput(String input) {
+        if (isNotInteger(input)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private static boolean isNotInteger(String input) {
+        try {
+            toInt(input);
+            return true;
+        } catch (NumberFormatException exception) {
+            return false;
+        }
+    }
 }
