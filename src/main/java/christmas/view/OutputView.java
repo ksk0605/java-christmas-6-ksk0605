bfb02a0bbf181a958a40f442ea38c4ae707cd187
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 03:43:57 +0900

    feat(OutputView): 증정 메뉴 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -18,7 +18,20 @@ public class OutputView {
     public static void printOrderAmount(Order order) {
         System.out.println("\n<할인 전 총주문 금액>\n" + formatPrizeAmount(order) + "원");
     }
-    
+
+    public static void printEventItem(Order order) {
+        System.out.println("\n<증정 메뉴>");
+        String eventItem = createEventItem(order);
+        System.out.println(eventItem);
+    }
+
+    private static String createEventItem(Order order) {
+        if (order.calculateOrderAmount() < 120000) {
+            return "없음";
+        }
+        return "샴페인 1개";
+    }
+
     private static String formatPrizeAmount(Order order) {
         return NumberFormat.getIntegerInstance().format(order.calculateOrderAmount());
     }
