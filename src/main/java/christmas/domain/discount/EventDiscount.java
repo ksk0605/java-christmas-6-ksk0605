1c4fad228d5ae03db8ec2c2ff8714dee2851ace8
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 00:30:20 +0900

    refactor(EventDiscount): public 함수 분리로 계산 로직 개선

--- src/main/java/christmas/domain/discount/EventDiscount.java
+++ src/main/java/christmas/domain/discount/EventDiscount.java
@@ -4,9 +4,13 @@ import christmas.domain.order.Order;
 
 public class EventDiscount implements Discount {
 
+    private static boolean isNotEligibleForEvent(Order order) {
+        return order.calculateOrderAmount() < 120000;
+    }
+
     @Override
     public int calculateDiscountAmount(Order order) {
-        if (order.calculateOrderAmount() < 120000) {
+        if (isNotEligibleForEvent(order)) {
             return 0;
         }
         return 25000; // TODO : static 상수 분리
