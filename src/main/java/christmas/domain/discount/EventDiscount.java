8a8b37b61bc7a51696fc0e7164f2c88387cbd84e
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 00:28:13 +0900

    feat(EventDiscount): 이벤트 혜택 할인 금액 계산 기능 구현

--- src/main/java/christmas/domain/discount/EventDiscount.java
+++ src/main/java/christmas/domain/discount/EventDiscount.java
@@ -1,2 +1,14 @@
-package christmas.domain.discount;public class EventDiscount {
+package christmas.domain.discount;
+
+import christmas.domain.order.Order;
+
+public class EventDiscount implements Discount {
+
+    @Override
+    public int calculateDiscountAmount(Order order) {
+        if (order.calculateOrderAmount() < 120000) {
+            return 0;
+        }
+        return 25000; // TODO : static 상수 분리
+    }
 }
