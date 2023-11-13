99927bef132d3219660ea287bc6311338ac22688
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 18:02:20 +0900

    feat(EventPlanner): 에러메시지 출력, 전체 프로세스 실행 기능 구현

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,7 +1,56 @@
 package christmas.controller;
 
-public class EventPlanner {
-    public void start() {
+import christmas.domain.VisitDate;
+import christmas.domain.discount.Discount;
+import christmas.domain.order.Order;
+import christmas.domain.order.OrderItems;
+import christmas.view.InputView;
+import christmas.view.OutputView;
 
+import java.util.List;
+
+public class EventPlanner {
+    private final List<Discount> discounts;
+
+    public EventPlanner(List<Discount> discounts) {
+        this.discounts = discounts;
+    }
+
+    public void start() {
+        VisitDate visitDate = createVisitDate();
+
+        Order order = createOrder(visitDate);
+
+        OutputView.printMenu(order);
+        OutputView.printOrderAmount(order);
+        OutputView.printEventItem(order);
+        OutputView.printDiscountDetails(discounts, order);
+        OutputView.printDiscountAmount(discounts, order);
+        OutputView.printExpectedPaymentAmount(discounts, order);
+        OutputView.printEventBadge(discounts, order);
+    }
+
+    private static Order createOrder(VisitDate visitDate) {
+        while (true) {
+            try {
+                String[] orders = InputView.readOrder();
+                OrderItems orderItems = new OrderItems(orders);
+
+                return new Order(orderItems, visitDate);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+    }
+
+    private static VisitDate createVisitDate() {
+        while (true) {
+            try {
+                int date = InputView.readDate();
+                return new VisitDate(date);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 }
