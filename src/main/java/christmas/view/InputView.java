7286d9ce8dd07be5e2ad84a42662ee844ab69ff4
SungKyum Kim <sungkyum1@naver.com>
2023-11-14 02:31:40 +0900

    refactor(View package): 출력 상수 분리

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -2,16 +2,19 @@ package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static christmas.view.ViewMessages.*;
+
 public class InputView {
     public static int readDate() {
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        System.out.println(WELCOME_MESSAGE);
+        System.out.println(ENTER_VISIT_DATE_PROMPT);
         String input = getUserInput();
         validateInputDate(input);
         return toInt(input);
     }
 
     public static String[] readOrder() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(ORDER_MENU_PROMPT);
         String input = getUserInput();
         validateInputOrder(input);
         return split(input);
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -11,34 +11,41 @@ import christmas.domain.order.OrderItems;
 import java.text.NumberFormat;
 import java.util.Optional;
 
+import static christmas.view.ViewMessages.*;
+
 public class OutputView {
+    public static void printEventHeader() {
+        System.out.println(EVENT_PREVIEW_HEADER);
+    }
+
     public static void printMenu(Order order) {
-        System.out.println("\n<주문 메뉴>");
+        System.out.println(ORDER_MENU_HEADER);
         OrderItems orderItems = order.getOrderItems();
         for (OrderItem orderItem : orderItems.getItems()) {
-            System.out.println(orderItem.getMenuItem() + " " + orderItem.getMenuCount() + "개");
+            System.out.println(orderItem.getMenuItem() + " " + orderItem.getMenuCount() + COUNT_SUFFIX);
         }
     }
 
     public static void printOrderAmount(Order order) {
-        System.out.println("\n<할인 전 총주문 금액>\n" + formatPrizeAmount(order.calculateOrderAmount()) + "원");
+        System.out.println(TOTAL_ORDER_AMOUNT_HEADER);
+        System.out.println(formatPrizeAmount(order.calculateOrderAmount()) + WON_SUFFIX);
     }
 
     public static void printEventItem(Order order) {
-        System.out.println("\n<증정 메뉴>");
+        System.out.println(BONUS_MENU_HEADER);
         EventItem eventItem = EventItem.from(order.calculateOrderAmount());
         if (eventItem.equals(EventItem.없음)) {
-            System.out.println("없음");
+            System.out.println(NO_DISCOUNT_MESSAGE);
             return;
         }
-        System.out.println(eventItem + " " + eventItem.getItemCount() + "개");
+        System.out.println(eventItem + " " + eventItem.getItemCount() + COUNT_SUFFIX);
     }
 
     public static void printDiscountDetails(Discounts discounts, Order order) {
-        System.out.println("\n<혜택 내역>");
+        System.out.println(DISCOUNT_DETAILS_HEADER);
 
         if (discounts.allDiscountsZero(order)) {
-            System.out.println("없음");
+            System.out.println(NO_DISCOUNT_MESSAGE);
             return;
         }
 
@@ -47,28 +54,28 @@ public class OutputView {
             if (discountAmount == 0) {
                 continue;
             }
-            System.out.println(discount.getDiscountName() + ": -" + formatPrizeAmount(discountAmount) + "원");
+            System.out.println(discount.getDiscountName() + COLON_SPACE + MINUS_SIGN + formatPrizeAmount(discountAmount) + WON_SUFFIX);
         }
     }
 
     public static void printDiscountAmount(Discounts discounts, Order order) {
-        System.out.println("\n<총혜택 금액>");
+        System.out.println(TOTAL_DISCOUNT_AMOUNT_HEADER);
         int discountAmount = discounts.sumAllDiscounts(order);
         if (discountAmount == 0) {
-            System.out.println("0원");
+            System.out.println(discountAmount + WON_SUFFIX);
             return;
         }
-        System.out.println("-" + formatPrizeAmount(discountAmount) + "원");
+        System.out.println(MINUS_SIGN + formatPrizeAmount(discountAmount) + WON_SUFFIX);
     }
 
     public static void printExpectedPaymentAmount(Discounts discounts, Order order) {
-        System.out.println("\n<할인 후 예상 결제 금액>");
+        System.out.println(FINAL_PAYMENT_AMOUNT_HEADER);
         int expectedPaymentAmount = order.calculateOrderAmount() + discounts.sumAllDiscountAmountExcludingEventDiscount(order);
-        System.out.println(formatPrizeAmount(expectedPaymentAmount) + "원");
+        System.out.println(formatPrizeAmount(expectedPaymentAmount) + WON_SUFFIX);
     }
 
     public static void printEventBadge(Discounts discounts, Order order) {
-        System.out.println("\n<12월 이벤트 배지>");
+        System.out.println(EVENT_BADGE_HEADER);
         int discountAmount = discounts.sumAllDiscounts(order);
         Optional<EventBadge> eventBadge = EventBadge.getBadgeForAmount(discountAmount);
         eventBadge.ifPresentOrElse(
