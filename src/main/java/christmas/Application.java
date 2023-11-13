eb4b04c5ea201c8bd52f28f551aeca92fd795129
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 18:02:46 +0900

    feat(Application): 전체 프로세스 실행 코드 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,21 @@
 package christmas;
 
+import christmas.controller.EventPlanner;
+import christmas.domain.discount.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Discount> discounts = new ArrayList<>();
+        discounts.add(new ChristmasDiscount());
+        discounts.add(new WeekdayDiscount());
+        discounts.add(new WeekendDiscount());
+        discounts.add(new SpecialDiscount());
+        discounts.add(new EventDiscount());
+
+        EventPlanner eventPlanner = new EventPlanner(discounts);
+        eventPlanner.start();
     }
 }
