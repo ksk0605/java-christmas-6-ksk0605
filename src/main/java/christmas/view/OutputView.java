5d4801e5a8d0cb3c1f52f8e670745ebd615cc957
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 16:53:42 +0900

    feat(OutputView): 혜택 내역 출력 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,12 @@
 package christmas.view;
 
+import christmas.domain.discount.Discount;
 import christmas.domain.order.Order;
 import christmas.domain.order.OrderItem;
 import christmas.domain.order.OrderItems;
 
 import java.text.NumberFormat;
+import java.util.List;
 
 public class OutputView {
     public static void printMenu(Order order) {
@@ -16,7 +18,7 @@ public class OutputView {
     }
 
     public static void printOrderAmount(Order order) {
-        System.out.println("\n<할인 전 총주문 금액>\n" + formatPrizeAmount(order) + "원");
+        System.out.println("\n<할인 전 총주문 금액>\n" + formatPrizeAmount(order.calculateOrderAmount()) + "원");
     }
 
     public static void printEventItem(Order order) {
@@ -25,14 +27,37 @@ public class OutputView {
         System.out.println(eventItem);
     }
 
-    private static String createEventItem(Order order) {
+    public static void printDiscountDetails(List<Discount> discounts, Order order) { // TODO : Discounts 클래스
+        System.out.println("\n<혜택 내역>");
+
+        if (hasNoDiscount(discounts, order)) {
+            System.out.println("없음");
+            return;
+        }
+
+        for (Discount discount : discounts) {
+            int discountAmount = discount.calculateDiscountAmount(order);
+            if (discountAmount == 0) {
+                continue;
+            }
+            System.out.println(discount.getDiscountName() + ": -" + formatPrizeAmount(discountAmount) + "원");
+        }
+    }
+
+    private static boolean hasNoDiscount(List<Discount> discounts, Order order) {
+        boolean hasDiscount = discounts.stream()
+                .anyMatch(discount -> discount.calculateDiscountAmount(order) > 0);
+        return !hasDiscount;
+    }
+
+    private static String createEventItem(Order order) { // TODO : 이벤트 아이템 enum으로 리팩토링
         if (order.calculateOrderAmount() < 120000) {
             return "없음";
         }
         return "샴페인 1개";
     }
 
-    private static String formatPrizeAmount(Order order) {
-        return NumberFormat.getIntegerInstance().format(order.calculateOrderAmount());
+    private static String formatPrizeAmount(int amount) {
+        return NumberFormat.getIntegerInstance().format(amount);
     }
 }
