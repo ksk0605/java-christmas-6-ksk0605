b2969ef1370b6e3d238eea88e54298a87c7b5675
SungKyum Kim <sungkyum1@naver.com>
2023-11-13 16:24:54 +0900

    fix(WeekdayDiscount): 할인 금액이 디저트 메뉴만 카운트 하지 않는 이슈 수정

--- src/main/java/christmas/domain/discount/WeekdayDiscount.java
+++ src/main/java/christmas/domain/discount/WeekdayDiscount.java
@@ -5,11 +5,11 @@ import christmas.domain.order.Order;
 public class WeekdayDiscount implements Discount {
 
     private static boolean isNotWeekDayDate(int visitDate) {
-        return visitDate % 7 != 1 && visitDate % 7 != 2;
+        return visitDate % 7 == 1 || visitDate % 7 == 2;
     }
 
     private static int calculateAmount(Order order) {
-        return 2023 * order.orderItemCounts();
+        return 2023 * order.getDessertOrderItemCountAmount();
     }
 
     @Override
@@ -21,5 +21,10 @@ public class WeekdayDiscount implements Discount {
         }
 
         return calculateAmount(order);
+    }
+
+    @Override
+    public String getDiscountName() {
+        return "평일 할인";
     }
 }
