b9912f9afb91e4bd4ea7cfcce14a35b2d0ef422b
SungKyum Kim <sungkyum1@naver.com>
2023-11-14 04:09:11 +0900

    test(WeekendDiscount): 테스트 케이스 수정

--- src/test/java/christmas/domain/discount/WeekendDiscountTest.java
+++ src/test/java/christmas/domain/discount/WeekendDiscountTest.java
@@ -14,7 +14,7 @@ class WeekendDiscountTest {
 
     @DisplayName("주말 할인 금액을 계산한다.")
     @ParameterizedTest
-    @CsvSource(value = {"샴페인-1,초코케이크-2:1:4046", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:3:0"}, delimiter = ':')
+    @CsvSource(value = {"바비큐립-1,초코케이크-2:3:0", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1:1:4046"}, delimiter = ':')
     void calculateDiscountAmount(String input, String date, int amount) {
         // given
         VisitDate visitDate = new VisitDate(date);
