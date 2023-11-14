1f498c697afc5e9d5a153addd7508ed7335f9773
SungKyum Kim <sungkyum1@naver.com>
2023-11-15 01:56:02 +0900

    test(Application): 날짜/주문 예외,없음 출력 테스트 케이스 추가

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -1,10 +1,13 @@
 package christmas;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ApplicationTest extends NsTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
@@ -14,38 +17,51 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() -> {
             run("3", "티본스테이크-1,바비큐립-1,초코케이크-2,제로콜라-1");
             assertThat(output()).contains(
-                "<주문 메뉴>",
-                "<할인 전 총주문 금액>",
-                "<증정 메뉴>",
-                "<혜택 내역>",
-                "<총혜택 금액>",
-                "<할인 후 예상 결제 금액>",
-                "<12월 이벤트 배지>"
+                    "<주문 메뉴>",
+                    "<할인 전 총주문 금액>",
+                    "<증정 메뉴>",
+                    "<혜택 내역>",
+                    "<총혜택 금액>",
+                    "<할인 후 예상 결제 금액>",
+                    "<12월 이벤트 배지>"
             );
         });
     }
 
-    @Test
-    void 혜택_내역_없음_출력() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "26:타파스-1,제로콜라-1:<혜택 내역>:없음",
+            "26:타파스-1,제로콜라-1:<증정 메뉴>:없음",
+            "26:타파스-1,제로콜라-1:<12월 이벤트 배지>:없음",
+    }, delimiter = ':')
+    void 결과_없음_출력(String date, String order, String outputTitle, String result) {
         assertSimpleTest(() -> {
-            run("26", "타파스-1,제로콜라-1");
-            assertThat(output()).contains("<혜택 내역>" + LINE_SEPARATOR + "없음");
+            run(date, order);
+            assertThat(output()).contains(outputTitle + LINE_SEPARATOR + result);
         });
     }
 
-    @Test
-    void 날짜_예외_테스트() {
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "-1", "0", "32", "%"})
+    void 날짜_예외_테스트(String input) {
         assertSimpleTest(() -> {
-            runException("a");
+            runException(input);
             assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         });
     }
 
-    @Test
-    void 주문_예외_테스트() {
+    @ParameterizedTest
+    @CsvSource(value = {
+            "3:제로콜라-a:[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+            "3:제로콜라-0:[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+            "3:제로콜라-21:[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.",
+            "3:제로콜라--2:[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+            "3:제로콜라/2:[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요."
+    }, delimiter = ':')
+    void 주문_예외_테스트(String date, String order, String errorMessage) {
         assertSimpleTest(() -> {
-            runException("3", "제로콜라-a");
-            assertThat(output()).contains("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            runException(date, order);
+            assertThat(output()).contains(errorMessage);
         });
     }
 
